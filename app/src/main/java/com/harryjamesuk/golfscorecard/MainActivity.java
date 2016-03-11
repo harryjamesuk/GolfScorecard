34460b577693790b3b32473c48327807cfa13d42
Harry James <harryjamesuk@users.noreply.github.com>
2016-03-11 18:31:55 +0000

    Added menu and Clear strokes functionality

--- app/src/main/java/com/harryjamesuk/golfscorecard/MainActivity.java
+++ app/src/main/java/com/harryjamesuk/golfscorecard/MainActivity.java
@@ -1,24 +1,77 @@
 package com.harryjamesuk.golfscorecard;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends ListActivity {
 
+    private static final String PREFS_FILE = "com.harryjamesuk.goldscorecard.preferences";
+    private static final String KEY_STROKECOUNT = "KEY_STROKECOUNT_";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     private Hole[] mHoles = new Hole[18];
+    private ListAdapter mListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
 
         // Initialize holes
         int strokes = 0;
         for (int i = 0; i < mHoles.length; i++) {
+            strokes = mSharedPreferences.getInt(KEY_STROKECOUNT + i, 0);
             mHoles[i] = new Hole("Hole " + (i+1) + " :", strokes);
         }
 
-        ListAdapter listAdapter = new ListAdapter(this, mHoles);
-        setListAdapter(listAdapter);
+        mListAdapter = new ListAdapter(this, mHoles);
+        setListAdapter(mListAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        
+        for (int i = 0; i < mHoles.length; i++) {
+            mEditor.putInt(KEY_STROKECOUNT + i, mHoles[i].getStrokeCount());
+        }
+        mEditor.apply();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_clear_strokes) {
+            mEditor.clear();
+            mEditor.apply();
+
+            for (Hole hole : mHoles) {
+                hole.setStrokeCount(0);
+            }
+            mListAdapter.notifyDataSetChanged();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
